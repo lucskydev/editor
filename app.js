eebd4783abdfadd0c95b388b4df376e14750a911
lucskydev <125794939+lucskydev@users.noreply.github.com>
2026-04-02 16:54:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+//import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // ===============================
 // SUPABASE v2
